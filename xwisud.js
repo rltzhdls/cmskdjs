9f65d4345ae5b06d83beff75ee60a4a7626c5695
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-13 13:23:57 +0900

    Add files via upload

---
+++ xwisud.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineage/shapechange20181011/intro.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
